00cec6f227d7f567e5dbdc73177dcfcff36dfcb5
Cho S. Kim <4cho.kim@gmail.com>
2014-06-07 11:17:31 -0700

    Change production URL from my personal URL to a generic URL

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://chochomon.azurewebsites.net/',
+        url: 'http://my-ghost-blog.com',
         mail: {},
         database: {
             client: 'sqlite3',
